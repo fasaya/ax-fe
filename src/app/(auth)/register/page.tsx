c469a82fad203760ba29879c76bbcf8766f5110f
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2024-06-16 17:13:35 +0800

    Create registration page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,180 @@
+'use client';
+
+import { navigate } from "@/utils/actions/navigate";
+import Link from "next/link"
+import Cookies from "js-cookie"
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { API_V1_URL } from "@/constant";
+
+const LoginPage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = async (data: any) => {
+        try {
+            const response = await fetch(API_V1_URL + '/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                throw new Error('Invalid email or password');
+            }
+
+            const responseData = await response.json();
+
+            // Logic to set cookie
+            // Set the expiration time to 1 hour from the current time
+            const expirationTime = new Date(
+                new Date().getTime() + 1 * 60 * 60 * 1000
+            );
+
+            // Set the "userToken" cookie with an expiration time
+            Cookies.set("userToken", responseData.data.token,
+                {
+                    expires: expirationTime,
+                    path: "/",
+                }
+            );
+
+            // Redirect to the dashboard or another protected page
+            navigate('/admin/product')
+        } catch (err) {
+            console.error(err);
+            // Handle error
+        }
+    };
+
+    return (
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    {/* <img
+                        className="mx-auto h-10 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                    /> */}
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                                Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    {...register('name', { required: 'Name is required' })}
+                                    id="name"
+                                    type="name"
+                                    autoComplete="name"
+                                    required
+                                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                {errors.name && <p>{errors.name.message?.toString()}</p>}
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                                Username
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    {...register('username', { required: 'Username is required' })}
+                                    id="username"
+                                    type="username"
+                                    autoComplete="username"
+                                    required
+                                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                {errors.username && <p>{errors.username.message?.toString()}</p>}
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    {...register('email', { required: 'Email is required' })}
+                                    id="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                {errors.email && <p>{errors.email.message?.toString()}</p>}
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Password
+                                </label>
+                                {/* <div className="text-sm">
+                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        Forgot password?
+                                    </a>
+                                </div> */}
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    {...register('password', { required: 'Password is required' })}
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                {errors.password && <p>{errors.password.message?.toString()}</p>}
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="c_password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Retype password
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="c_password"
+                                    {...register('c_password', { required: 'Retype password is required' })}
+                                    type="password"
+                                    required
+                                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                {errors.c_password && <p>{errors.c_password.message?.toString()}</p>}
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Not a member?{' '}
+                        <Link href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Register</Link>
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginPage
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,1 +1,1 @@
-export const API_V1_URL = 'http://127.0.0.1:8000'
+export const API_V1_URL = 'http://127.0.0.1:8000/api/v1'
